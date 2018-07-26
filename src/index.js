7bcc4973de96f2ed6810f567f0e4b3ab39699796
Scott Moon <scott@dogmate.co.kr>
2018-07-26 23:25:32 +0900

    [Step4] 리덕스 데브 툴 설치

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
 
 import {createEpicMiddleware} from 'redux-observable';
 import {rootEpic} from "./epics";
 
+
 const epicMiddleware = createEpicMiddleware();
-const store = createStore(reducer, applyMiddleware(epicMiddleware));
+const store = createStore(
+    reducer,
+	composeWithDevTools(
+	    applyMiddleware(epicMiddleware)
+    )
+);
 epicMiddleware.run(rootEpic);
 
 ReactDOM.render(
